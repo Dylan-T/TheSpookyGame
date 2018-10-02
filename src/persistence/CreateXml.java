ae2b5fe52452548b1f4a99c0ef5b68604d032e2a
hoongkevi <hoongkevi@purple-onion.ecs.vuw.ac.nz>
2018-10-02 15:37:27 +1300

    Created basic XML reader and writer

---
+++ src/persistence/CreateXml.java
@@ -0,0 +1,91 @@
+package persistence;
+
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+/**
+ * Creates an XML file which fits our games elements and description.
+ * @author hoongkevi
+ *
+ */
+public class CreateXml {
+  private static final String filePath = "/home/hoongkevi/game.xml";
+
+
+  /**
+   * creates the XML file by taking game attributes and creating elements.
+   * @param args main function.
+   * @throws ParserConfigurationException parser exception.
+   * @throws TransformerException transformer exception.
+   */
+  public static void main(String[] args) throws ParserConfigurationException, TransformerException {
+    try {
+      DocumentBuilderFactory df = DocumentBuilderFactory.newInstance();
+      DocumentBuilder db = df.newDocumentBuilder();
+      Document document = db.newDocument();
+
+      Element game = document.createElement("Game");
+      document.appendChild(game);
+
+      Element player = document.createElement("Player");
+      game.appendChild(player);
+
+      Element inventory = document.createElement("Inventory");
+      player.appendChild(inventory);
+
+      Attr attr = document.createAttribute("health");
+      attr.setValue("100");
+      player.setAttributeNode(attr);
+
+      Attr attr1 = document.createAttribute("x");
+      attr1.setValue("0");
+      player.setAttributeNode(attr1);
+
+      Attr attr2 = document.createAttribute("y");
+      attr2.setValue("0");
+      player.setAttributeNode(attr2);
+
+      Attr attr3 = document.createAttribute("room");
+      attr3.setValue("street");
+      player.setAttributeNode(attr3);
+
+      Element durries = document.createElement("durries");
+      inventory.appendChild(durries);
+
+      TransformerFactory tf = TransformerFactory.newInstance();
+      Transformer transformer = tf.newTransformer();
+      DOMSource ds = new DOMSource(document);
+      StreamResult sr = new StreamResult(new File(filePath));
+      transformer.transform(ds,sr);
+      System.out.println("XML File Created");
+    } catch (ParserConfigurationException e) {
+      e.printStackTrace();
+    } catch (TransformerException e) {
+      e.printStackTrace();
+    }
+  }
+
+
+
+  /**
+   * gets the file path.
+   * @return filepath
+   */
+  public static String getFilepath() {
+    return filePath;
+  }
+
+}
