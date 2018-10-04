9ced82a55d86a71bff4928e2bbd2f053986a9c4c
Niran <Niran@192.168.1.70>
2018-10-04 15:26:19 +1300

    New constructor which takes in a Graphics2D object

--- src/renderer/Renderer.java
+++ src/renderer/Renderer.java
@@ -1,7 +1,10 @@
 package renderer;
 
+import java.awt.Graphics2D;
+
 import gameworld.GameWorld;
 import gameworld.GameWorld.Direction;
+import gameworld.Item;
 import gameworld.Location;
 
 /**
@@ -10,14 +13,31 @@ import gameworld.Location;
  */
 public class Renderer {
   
+  Graphics2D graphics;
+  
+  /**
+   * @param graphics
+   * sets up the graphics for the items and exits to be drawn from
+   */
+  public Renderer(Graphics2D graphics) {
+    this.graphics = graphics;
+  }
+  
   /**
    * @param room
    * @param dir
+   * @param g 
    */
-  public void redraw(Location room, GameWorld.Direction dir) {
+  public void redraw(Location room, GameWorld.Direction dir, Graphics2D g) {
+    
+    Item[][] grid = room.getGrid();
     
     if(dir == Direction.NORTH) {
-      
+      for(int i = 0; i< grid.length; i++) {
+        for(int j = 0; j< grid[0].length; j++) {
+          
+        }
+      }
     }
     
   }
