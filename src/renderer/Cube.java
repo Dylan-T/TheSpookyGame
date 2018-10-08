b7d6fa075e426c45df1ad529ec2f0727e4065efa
Niran <Niran@192.168.1.70>
2018-10-09 00:39:25 +1300

    new 3D room

--- src/renderer/Cube.java
+++ src/renderer/Cube.java
@@ -1,5 +1,6 @@
 package renderer;
 
+import java.awt.Rectangle;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,8 +13,15 @@ public class Cube {
   int x;
   int y;
   int size;
+  int x2;
+  int y2;
+  int size2;
   int Xgap;
   int Ygap;
+  float scale;
+  
+  Rectangle rec1;
+  Rectangle rec2;
   
   ArrayList<Point> square1Points;
   ArrayList<Point> square2Points;
@@ -22,20 +30,37 @@ public class Cube {
    * @param x
    * @param y
    * @param size
+   * @param x2 
+   * @param y2 
+   * @param size2 
    * @param Xgap 
    * @param Ygap 
+   * @param scale 
    * @param gap
    */
-  public Cube(int x, int y, int size, int Xgap, int Ygap) {
+  public Cube(int x, int y, int size, int x2, int y2, int size2) {
 
     this.x = x;
     this.y = y;
     this.size = size;
-    this.Xgap = Xgap;
-    this.Ygap = Ygap;
+    this.x2 = x2;
+    this.y2 = y2;
+    this.size2 = size2;
     square1Points = getSquare1Points();
     square2Points = getCube2Points();
+    
 
+    
+  }
+  
+  /**
+   * @param x
+   * @param y 
+   */
+  public Cube(Rectangle x, Rectangle y ) {
+    this.rec1 = x;
+    this.rec2 = y;
+    
   }
 
   /**
@@ -55,13 +80,13 @@ public class Cube {
    * @return the list of points that make up the second square that is shifted
    */
   public ArrayList<Point> getCube2Points() {
-    int newX = x + Xgap;
-    int newY = y + Ygap;
+    int newX = x2;
+    int newY = y2;
     ArrayList<Point> points = new ArrayList<Point>();
     points.add(new Point(newX, newY));
-    points.add(new Point(newX + size, newY));
-    points.add(new Point(newX + size, newY + size));
-    points.add(new Point(newX, newY + size));
+    points.add(new Point(newX + size2, newY));
+    points.add(new Point(newX + size2, newY + size2));
+    points.add(new Point(newX, newY + size2));
     return points;
   }
   
--- src/renderer/Renderer.java
+++ src/renderer/Renderer.java
@@ -5,6 +5,7 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Polygon;
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -30,11 +31,11 @@ public class Renderer {
   /**
    * width of the canvas
    */
-  public static final int CANVASWIDTH = 900;
+  public static final int CANVASWIDTH = 1604;
   /**
    * height of the canvas
    */
-  public static final int CANVASHEIGHT = 750;
+  public static final int CANVASHEIGHT = 951;
   
   Graphics graphics;
   
@@ -98,7 +99,7 @@ public class Renderer {
 
     BufferedImage cube = null;
     try {
-        cube = ImageIO.read(new File("black cubde.jpg"));
+        cube = ImageIO.read(new File("door.png"));
     } catch (IOException e) {
     }
     //System.out.println(cube);
@@ -106,33 +107,99 @@ public class Renderer {
     
     if(dir == Direction.NORTH) {
       //x = 50* (1-scale);
-      for(int i = 0; i< 20; i++) {
+      int xgap = 2;
+      int ygap = 2;
+      
+      int yMiddle = CANVASHEIGHT/2;
+      int xMiddle = CANVASWIDTH/2;
+      
+      
+      
+      g.drawRect(xMiddle - 100, yMiddle - 50, 200, 100);
+      g.drawRect(0, 0, CANVASWIDTH, CANVASHEIGHT);
+      
+      g.drawLine(xMiddle - 100, yMiddle - 50, 0, 0);
+      g.drawLine(xMiddle + 100, yMiddle - 50, CANVASWIDTH, 0);
+      g.drawLine(xMiddle - 100, yMiddle + 50, 0, CANVASHEIGHT);
+      g.drawLine(xMiddle + 100, yMiddle + 50, CANVASWIDTH, CANVASHEIGHT);
+      
+      int widthScale = CANVASWIDTH/200;
+      int heightScale = CANVASHEIGHT/100;
+      
+      int tempY = 0;
+      int tempX = 0;
+      
+      for(int i = yMiddle-50; i<yMiddle + 50; i+=1) {
+        tempY += 1*heightScale;
+        g.drawLine(xMiddle + 100, i, CANVASWIDTH, tempY);
+      }
+      
+      tempY = 0;
+      
+      for(int i = yMiddle-50; i<yMiddle + 50; i+=1) {
+        tempY += 1*heightScale;
+        g.drawLine(xMiddle - 100, i, 0, tempY);
+      }
+      
+      for(int i = xMiddle-100; i<xMiddle + 100; i+=1) {
+        tempX += 1*widthScale;
+        g.drawLine(i, yMiddle + 50, tempX, CANVASHEIGHT);
+      }
+      
+      tempX = 0;
+      
+      for(int i = xMiddle-100; i<xMiddle + 100; i+=1) {
+        tempX += 1*widthScale;
+        g.drawLine(i, yMiddle - 50, tempX, 0);
+      }
+      
+     /* g.drawRect(xMiddle + 90, yMiddle + 40, 10, 10);
+      g.drawRect(CANVASWIDTH - 10*widthScale, CANVASHEIGHT - 10*heightScale, 10*widthScale, 10*heightScale);*/
+      this.fillCube(new Cube(xMiddle + 90, yMiddle + 40, 10, CANVASWIDTH - 10*widthScale, CANVASHEIGHT - 10*heightScale, 10*widthScale), g);
+      this.fillCube(new Cube(xMiddle - 100, yMiddle + 40, 10, 0 + 10*widthScale, CANVASHEIGHT - 10*heightScale, 10*widthScale), g);
+      
+      
+      g.drawImage(cube.getScaledInstance(30, 40, Image.SCALE_DEFAULT), xMiddle-10, yMiddle+10, null);
+      
+      //g.drawRect(xMiddle-10, yMiddle+10, 20, 40);
+      /*for(int i = 0; i< 20; i++) {
 
-       /* g.setColor(colors.get(colorNumber));
-        colorNumber++;*/
+        g.setColor(colors.get(colorNumber));
+        colorNumber++;
      
         for(int j = 0; j< 20; j++) {
           //Image current = grid[i][j].getImage();
           //cube.getScaledInstance(Math.round(cube.getWidth(null)*scale), Math.round(cube.getHeight(null)*scale), Image.SCALE_DEFAULT);
           //g.drawImage(cube, x, y, null); 
-          fillCube(new Cube(x, y, Math.round(width*scale), Math.round(height*scale), Math.round(height*scale)),g);
+          Cube temp = new Cube(x, y, Math.round(width*scale), Math.round(scale), Math.round(scale), scale);
+          fillCube(temp, g);
           
           //g.setColor(Color.BLUE);
           //g.fillRect(x, y, Math.round(width*scale), Math.round(height*scale));
           //g.fill3DRect(x, y, Math.round(width*scale), Math.round(height*scale), true);
-          x += 50*scale;
+          
+          //x += 50*scale;
+          
+          x = x + temp.Xgap;
+          y = y + temp.Ygap;
           
           
         }
-        scale += 0.1;
-        x = 10;
-        y += 50;
+        scale += 0.2;
+        //x = 10;
+        //y += 50;
         
-      }
+      }*/
+      
     }
+    
+    
+    
+    //for()
+    
     //g.setColor(Color.BLUE);
     
-    if(dir == Direction.SOUTH) {
+    /*if(dir == Direction.SOUTH) {
       for(int i = grid.length-1; i>= 0; i++) {
         for(int j = grid[0].length-1; j>=0; j++) {
           
@@ -167,7 +234,8 @@ public class Renderer {
       }
     }
     
-  } 
+  } */
+  }
   
   /**
    * draws the cube onto the canvas byy connecting vertices
@@ -250,22 +318,34 @@ public class Renderer {
     rect4.addPoint(square1.get(2).getX(), square1.get(2).getY());
     
        
-    g.setColor(Color.DARK_GRAY);
+    g.setColor(Color.BLACK);
     
     g.fillPolygon(rect1);
+    
     
     g.fillPolygon(rect2);
     
     g.fillPolygon(rect3);
-    
+    g.setColor(Color.GRAY);
     g.fillPolygon(rect4);
     
-    g.setColor(Color.GRAY);
-    
-    g.fillRect(square2.get(0).getX(), square2.get(0).getY(), c.size, c.size);
     
     
+    g.fillRect(square2.get(0).getX(), square2.get(0).getY(), c.size2, c.size2);
     
+    
+    
+  }
+  
+  /**
+   * @param c
+   * @param g
+   */
+  public void fillCubeR(Cube c, Graphics g){
+    Rectangle rec1 = c.rec1;
+    Rectangle rec2 = c.rec2;
+    
+    g.drawLine(rec1.x, rec1.y, rec2.x, rec2.y);
   }
   
   public String toString() {
